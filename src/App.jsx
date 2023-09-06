ddefb240081d1c6ecba567666416e16033be6dc5
Aaishika S Bhattacharya <52964353+aaishikasb@users.noreply.github.com>
2023-09-06 23:29:38 +0000

    Add Changes

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  name: "Aaishika S Bhattacharya",
+  title: "Developer Advocate",
+  email: "contact@email.com",
+  gitHub: "aaishikasb",
+  instagram: "putaatoboii",
+  linkedIn: "aaishika",
+  medium: "aaishika",
+  twitter: "aaishika",
+  youTube: "@Aaishika",
 };
 
 const primaryColor = "#4E567E";
